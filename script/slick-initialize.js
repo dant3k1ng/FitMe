421c8dea2c65c217ad50034d3b9af4048550d4af
dant3k1ng <stas100399@interia.pl>
2023-02-15 18:27:14 +0100

    fixes

--- script/slick-initialize.js
+++ script/slick-initialize.js
@@ -1,4 +1,5 @@
 $(document).ready(function(){
+
         $('.diet-carousel').slick({
             dots: true,
             arrows: false,
@@ -35,93 +36,93 @@ $(document).ready(function(){
               }
             ]
           });
-    });
 
-    $('.meals-carousel').slick({
-      slidesToShow: 3,
-      slidesToScroll: 1,
-      responsive: [
-        {
-          breakpoint: 1080,
-          settings: {
-            arrows: false,
-            slidesToShow: 3,
-            slidesToScroll: 1
+      $('.meals-carousel').slick({
+        slidesToShow: 3,
+        slidesToScroll: 1,
+        responsive: [
+          {
+            breakpoint: 1080,
+            settings: {
+              arrows: false,
+              slidesToShow: 3,
+              slidesToScroll: 1
+            }
+          },
+          {
+            breakpoint: 820,
+            settings: {
+              arrows: false,
+              slidesToShow: 2,
+              slidesToScroll: 1
+            }
+          },
+          {
+            breakpoint: 720,
+            settings: {
+              arrows: false,
+              slidesToShow: 1,
+              slidesToScroll: 1
+            }
           }
-        },
-        {
-          breakpoint: 820,
-          settings: {
-            arrows: false,
-            slidesToShow: 2,
-            slidesToScroll: 1
-          }
-        },
-        {
-          breakpoint: 720,
-          settings: {
-            arrows: false,
-            slidesToShow: 1,
-            slidesToScroll: 1
-          }
-        }
-      ]
-    });
+        ]
+      });
 
-    $('.workout-carousel').slick({
-      dots: true,
-      arrows: false,
-      infinite: true,
-      speed: 300,
-      slidesToShow: 4,
-      slidesToScroll: 4,
-      autoplay: true,
-      autoplaySpeed: 5000,
-      responsive: [
-        {
-          breakpoint: 1080,
-          settings: {
-            arrows: false,
-            centerMode: true,
-            centerPadding: '40px',
-            slidesToShow: 2,
-            slidesToScroll: 2
+      $('.workout-carousel').slick({
+        dots: true,
+        arrows: false,
+        infinite: true,
+        speed: 300,
+        slidesToShow: 4,
+        slidesToScroll: 4,
+        autoplay: true,
+        autoplaySpeed: 5000,
+        responsive: [
+          {
+            breakpoint: 1080,
+            settings: {
+              arrows: false,
+              centerMode: true,
+              centerPadding: '40px',
+              slidesToShow: 2,
+              slidesToScroll: 2
+            }
+          },
+          {
+            breakpoint: 620,
+            settings: {
+              arrows: false,
+              slidesToShow: 1,
+              slidesToScroll: 1
+            }
+          },
+          {
+            breakpoint: 480,
+            settings: {
+              arrows: false,
+              slidesToShow: 1,
+              slidesToScroll: 1
+            }
+          },
+          {
+            breakpoint: 320,
+            settings: {
+              arrows: false,
+              slidesToShow: 1,
+              slidesToScroll: 1
+            }
           }
-        },
-        {
-          breakpoint: 620,
-          settings: {
-            arrows: false,
-            slidesToShow: 1,
-            slidesToScroll: 1
-          }
-        },
-        {
-          breakpoint: 480,
-          settings: {
-            arrows: false,
-            slidesToShow: 1,
-            slidesToScroll: 1
-          }
-        },
-        {
-          breakpoint: 320,
-          settings: {
-            arrows: false,
-            slidesToShow: 1,
-            slidesToScroll: 1
-          }
-        }
-      ]
-    });
+        ]
+      });
 
-    $('.showroom-carousel').slick({
-      slidesToShow: 1,
-      slidesToScroll: 1,
-      autioplay: true,
-      dots: true,
-      infinite: true,
-      speed: 500,
-      fade: true,
-      cssEase: 'linear'
-    })
+      $('.showroom-carousel').slick({
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        autioplay: true,
+        dots: true,
+        infinite: true,
+        speed: 500,
+        fade: true,
+        cssEase: 'linear'
+      });
+});
